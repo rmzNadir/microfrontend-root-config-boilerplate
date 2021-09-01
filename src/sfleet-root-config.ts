d9ffdb93af0f6756bf2bc6fdcc170ce49939ff9f
rmzNadir <dgzrz99@gmail.com>
2021-09-01 16:36:26 -0500

    :construction:

--- src/sfleet-root-config.ts
+++ src/sfleet-root-config.ts
@@ -2,11 +2,11 @@ import singleSPA, { registerApplication, start } from 'single-spa';
 import i18next from 'i18next';
 import { initReactI18next } from 'react-i18next';
 
+// Ideally this would be in the parcel that holds the app menu, this is just a config example.
 const defLng = localStorage.getItem('i18next');
 
 i18next.use(initReactI18next).init({
   lng: defLng ? JSON.parse(defLng) : 'es-MX',
-  fallbackLng: 'es-MX',
   interpolation: {
     escapeValue: false, // react already safes from xss
   },
@@ -24,7 +24,7 @@ registerApplication({
   name: '@sfleet/microfront',
   app: () =>
     System.import('@sfleet/microfront') as Promise<singleSPA.LifeCycles<never>>,
-  activeWhen: (location: any) => location.pathname === '/',
+  activeWhen: (location: Location) => location.pathname === '/',
   customProps: { i18n: i18next } as any, // Typings for this aren't working correctly
 });
 
